bbd0b304c260d076ba8fa379aaccc82af7c3e316
Jeff Prosise <jeffpro@wintellect.com>
2023-05-16 11:17:56 +0100

    Add Azure ChatGPT example

--- Azure Cognitive Services/Data/Program-commented.cs
+++ Azure Cognitive Services/Data/Program-commented.cs
@@ -1,60 +1,57 @@
-// Import necessary namespaces
+// Import necessary libraries
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using System;
 using System.Linq;
 
-// Define namespace and class
 namespace SentimentAnalysis
 {
     class Program
     {
-        // Define paths for loading and saving data
+        // Define the paths to load and save the data
         static readonly string _loadPath = "..\\..\\..\\Data\\yelp_labelled.tsv";
         static readonly string _savePath = "..\\..\\..\\Data\\Sentiment.zip";
 
-        // Main method for program execution
+        // Main function
         static void Main(string[] args)
         {
-            // Create new MLContext object for machine learning operations
+            // Create MLContext
             var context = new MLContext(seed: 0);
 
-            // Load data from file into Input object
+            // Load data from the input file
             var data = context.Data.LoadFromTextFile<Input>(_loadPath, hasHeader: false);
 
-            // Split data into test and train datasets
+            // Split the data into train and test datasets
             var trainTestData = context.Data.TrainTestSplit(data, testFraction: 0.2, seed: 0);
             var trainData = trainTestData.TrainSet;
             var testData = trainTestData.TestSet;
 
-            // Define machine learning pipeline for sentiment analysis
+            // Build the pipeline
             var pipeline = context.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: "SentimentText")
                 .Append(context.BinaryClassification.Trainers.FastTree(numberOfLeaves: 50, minimumExampleCountPerLeaf: 20));
 
-            // Train model using training dataset
+            // Train the model
             Console.WriteLine("Training the model...");
             var model = pipeline.Fit(trainData);
 
-            // Test model using testing dataset
+            // Use the model to make predictions on test data and evaluate the performance
             var predictions = model.Transform(testData);
-
-            // Evaluate model performance
             var metrics = context.BinaryClassification.Evaluate(predictions, "Label");
 
-            // Print evaluation results
+            // Print evaluation metrics
             Console.WriteLine();
             Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
             Console.WriteLine($"AUC: {metrics.AreaUnderPrecisionRecallCurve:P2}");
             Console.WriteLine($"F1: {metrics.F1Score:P2}");
             Console.WriteLine();
 
-            // Save trained model for future use
+            // Save the trained model
             Console.WriteLine("Saving the model...");
             context.Model.Save(model, data.Schema, _savePath);
         }
     }
 
-    // Define Input and Output classes for loading and processing data
+    // Define input class structure
     public class Input
     {
         [LoadColumn(0)]
@@ -64,6 +61,7 @@ namespace SentimentAnalysis
         public bool Sentiment;
     }
 
+    // Define output class structure
     public class Output
     {
         [ColumnName("PredictedLabel")]
--- Azure Cognitive Services/Data/app-commented.py
+++ Azure Cognitive Services/Data/app-commented.py
@@ -1,84 +1,89 @@
-# import necessary packages
 import os, pickle
 from flask import Flask, render_template, request
 from transformers import TFAutoModel, AutoTokenizer, TFAutoModelForQuestionAnswering, pipeline
 import numpy as np
 
-# initialize Flask app and secret key
+# Create Flask app and set secret key
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-# define the BERT-based language model and tokenizer
+# Initialize BERT and Question Answering models
 bert_id = 'sebastian-hofstaetter/distilbert-dot-margin_mse-T2-msmarco'
 bert_tokenizer = AutoTokenizer.from_pretrained(bert_id) 
 bert_model = TFAutoModel.from_pretrained(bert_id, from_pt=True)
 
-# define the question-answering model and tokenizer
 qa_id = 'deepset/minilm-uncased-squad2'
 qa_tokenizer = AutoTokenizer.from_pretrained(qa_id)
 qa_model = TFAutoModelForQuestionAnswering.from_pretrained(qa_id, from_pt=True)
 qa_pipe = pipeline('question-answering', model=qa_model, tokenizer=qa_tokenizer)
 
-# load preprocessed contexts
+# Load preprocessed contexts
 contexts = pickle.load(open('contexts.pkl', 'rb'))
 
-# define a function for vectorizing text
+# Helper function to convert text to a vector
 def vectorize_text(text):
-    tokenized_text = bert_tokenizer(text[:512], return_tensors='tf') # tokenize the text input, truncating to 512 tokens or less
-    vectorized_text = bert_model(tokenized_text)[0][:, 0, :][0] # feed the tokenized text into the language model and extract the first (CLS) token's output vector as a representation of the text
+    tokenized_text = bert_tokenizer(text[:512], return_tensors='tf') # tokenize text
+    vectorized_text = bert_model(tokenized_text)[0][:, 0, :][0] # get BERT encoding
     return vectorized_text
 
-# define a function for calculating cosine similarity between two vectors
+# Helper function to calculate cosine similarity between two vectors
 def vector_similarity(x, y):
     return np.dot(np.array(x), np.array(y))
 
-# define a function for ranking contexts based on similarity to a given query
+# Helper function to order contexts by similarity to query
 def order_contexts_by_query_similarity(query, contexts):
-    query_embedding = vectorize_text(query).numpy() # vectorize the query using the defined function
+    query_embedding = vectorize_text(query).numpy() # convert query to BERT vector
 
     similarities = sorted([
-        (vector_similarity(query_embedding, embedding), text) for text, embedding in contexts # calculate cosine similarity between the query embedding and each context
-    ], reverse=True) # sort the similarities in descending order
+        (vector_similarity(query_embedding, embedding), text) for text, embedding in contexts # calculate cosine similarity for each context
+    ], reverse=True) # sort contexts by descending similarity 
 
     return similarities
 
-# define a function for answering a given query
+# Function to answer a given query
 def answer_query(query, max_items=3):
     best_score = 0.0
     best_context = None
     best_start = 0
     best_end = 0
 
-    best_contexts = order_contexts_by_query_similarity(query, contexts)[:max_items] # rank the contexts using the defined function and keep only the top max_items    
+    best_contexts = order_contexts_by_query_similarity(query, contexts)[:max_items] # get top contexts based on similarity to query
 
+    # Loop through top contexts and use QA model to find answer
     for similarity, text in best_contexts:
-        result = qa_pipe(question=query, context=text, handle_impossible_answer=True) # use the question answering pipeline to find the answer to the query within the context
+        result = qa_pipe(question=query, context=text, handle_impossible_answer=True)
 
-        if result['start'] != result['end']: # check that an answer was found
+        # If answer is found, update best answer
+        if result['start'] != result['end']:
             score = result['score']
 
-            if score > best_score: # update the best answer if a higher score is found
+            if score > best_score:
                 best_score = score
                 best_context = text
                 best_start = result['start']
                 best_end = result['end']
 
-    if best_score > 0.0: # if a non-zero best score was found, return the answer with highlighted text
+    # If answer is found, format it with HTML tags and return
+    if best_score > 0.0:
         return f'{best_context[:best_start]}<mark>{best_context[best_start:best_end]}' \
                f'</mark>{best_context[best_end:]} ({best_score:.1%})'
-    else: # otherwise, return 'I don't know'
+    # Otherwise, return "I don't know"
+    else:
         return 'I don\'t know'
     
-# define a Flask route for the home page
+# Define route for index page
 @app.route('/', methods=['GET'])
 def index():
     output = ''
-    query = request.args.get('query')
+    query = request.args.get('query') # get query string from URL parameter
 
+    # If no query is given, set query to empty string
     if query is None:
         query = ''
 
+    # Otherwise, try to answer the query
     else:
         output = answer_query(query)
 
-    return render_template('index.html', query=query, answer=output) # render the HTML template with the query and answer variables
+    # Render index HTML template with query and answer
+    return render_template('index.html', query=query, answer=output)
